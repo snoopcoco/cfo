b95c173cdbd3de962c84bd390701640c5a1d9061
snoopcoco <snoop5572@gmail.com>
2016-03-23 04:22:54 +0000

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323022316) do
+ActiveRecord::Schema.define(version: 20160323042129) do
 
   create_table "pj_types", force: :cascade do |t|
     t.string   "name"
@@ -26,5 +26,15 @@ ActiveRecord::Schema.define(version: 20160323022316) do
     t.datetime "updated_at", null: false
     t.string   "pj_type"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
