33af09ccec4c6c7ffc2bbab22577047a879aed6b
snoopcoco <snoop5572@gmail.com>
2016-03-23 04:16:28 +0000

    create project

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323010442) do
+ActiveRecord::Schema.define(version: 20160323022316) do
+
+  create_table "pj_types", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "pj_name"
