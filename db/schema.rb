d7b8c9a67ee68b73ffaaa737625cf99b00f2f965
snoopcoco <snoop5572@gmail.com>
2016-03-24 05:50:46 +0000

    add work model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160323042129) do
+ActiveRecord::Schema.define(version: 20160324054336) do
+
+  create_table "add_project_id_to_tasks", force: :cascade do |t|
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pj_types", force: :cascade do |t|
     t.string   "name"
@@ -36,5 +42,15 @@ ActiveRecord::Schema.define(version: 20160323042129) do
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+
+  create_table "works", force: :cascade do |t|
+    t.string   "wkname"
+    t.boolean  "done"
+    t.integer  "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "works", ["project_id"], name: "index_works_on_project_id"
 
 end
