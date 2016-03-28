4c1fa333936b0154c2660e6ab9d249b1eb3089d2
snoopcoco <snoop5572@gmail.com>
2016-03-28 00:55:06 +0000

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160323074727) do
-=======
 ActiveRecord::Schema.define(version: 20160324075234) do
 
   create_table "add_project_id_to_tasks", force: :cascade do |t|
@@ -21,7 +18,6 @@ ActiveRecord::Schema.define(version: 20160324075234) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
->>>>>>> login
 
   create_table "pj_types", force: :cascade do |t|
     t.string   "name"
